ae282bf7e2309194f5deaa1df3c830a94d20c09f
Richard Tinajero <rj.tinajero92@gmail.com>
2020-08-06 15:20:06 -0700

    Updates

---
+++ precache-manifest.f617c6e5571ef902e17ebda4e07a556d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e5ebe155a65a53e5c71beca352aa72e",
+    "url": "/redux-todo/index.html"
+  },
+  {
+    "revision": "7384667b670200e93607",
+    "url": "/redux-todo/static/css/main.c8e1f734.chunk.css"
+  },
+  {
+    "revision": "d0ec0814ab5affd21290",
+    "url": "/redux-todo/static/js/2.b393a1a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/redux-todo/static/js/2.b393a1a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7384667b670200e93607",
+    "url": "/redux-todo/static/js/main.cb7b4325.chunk.js"
+  },
+  {
+    "revision": "1751eb7adb230102e216",
+    "url": "/redux-todo/static/js/runtime-main.0b6e7e65.js"
+  }
+]);
