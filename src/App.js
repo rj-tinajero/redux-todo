8361dd50ee5dc2224104cc530a6265e4db0bb8bd
Richard Tinajero <rj.tinajero92@gmail.com>
2020-08-06 15:17:48 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+	    <div id="iphone">
+		    <iframe border="0" src="https://rj-tinajero.github.io/Redux-To-Do-List/" width="375" height="812"></iframe>
+	    </div>
     </div>
   );
 }
